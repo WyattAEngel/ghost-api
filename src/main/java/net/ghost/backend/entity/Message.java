0a6ba2f35e58f2355c4c5e39239b5dd365aa14d9
Wyatt <wyattaengel@gmail.com>
2025-08-09 12:09:05 -0500

    Updating classes and adding config settings for mysql

---
+++ src/main/java/net/ghost/backend/entity/Message.java
@@ -0,0 +1,30 @@
+package net.ghost.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private Long content;
+
+    @Column(name = "datetime")
+    private String datetime;
+
+    @Column(name = "user")
+    private String user;
+
+}
